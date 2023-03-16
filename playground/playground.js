956c60055371c7826faf70f57d5be574de4b95e3
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-16 13:08:30 -0500

    Builds scroll to accept html and js file

---
+++ playground/playground.js
@@ -0,0 +1,20 @@
+function isSubsequence(s, t) {
+    let count = 0;
+    // Look at chars in s and find them in sequence in t
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i];
+        for (let j = 0; j < t.length; j++) {
+            const charT = t[j];
+            // Count the matches and compare with s length
+            if (charS === charT) count++;
+        }
+    }
+    // If we don't make it all the way, return false
+    if (count !== s.length) return false;
+    // Default return true if checks pass
+    return true;
+}
+
+const s = "abc";
+const t = "ahbgdc";
+console.log(isSubsequence(s, t));
