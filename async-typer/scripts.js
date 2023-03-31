a95756dace0e950e8bec99043d82d572e562cd8b
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-31 14:04:22 -0500

    Makes a function that types headings and paragraph at different speeds

--- async-typer/scripts.js
+++ async-typer/scripts.js
@@ -1,13 +1,23 @@
-function wait(ms) {
+function wait(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function getRandomBetween(min, max, randomNumber = Math.random()) {
-  return Math.floor(randomNumber * (max - min + 1)) + min;
+function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
+  return Math.floor(randomNumber * (max - min) + min);
 }
 
-function draw(element) {
-  console.log(element);
+//   async for of loop
+async function draw(el) {
+  const text = el.textContent;
+  let soFar = '';
+  for (const letter of text) {
+    soFar += letter;
+    el.textContent = soFar;
+    // wait for some amount of time
+    const { typeMin, typeMax } = el.dataset;
+    const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+    await wait(amountOfTimeToWait);
+  }
 }
 
 document.querySelectorAll('[data-type]').forEach(draw);
