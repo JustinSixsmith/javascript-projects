c9f6aa38379211c7de4ca381bd09585ed12a2a76
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-04-04 11:27:49 -0500

    Converts currencies

--- Wes Bos Exercises/Currency Converter/money.js
+++ Wes Bos Exercises/Currency Converter/money.js
@@ -1,5 +1,18 @@
 const fromSelect = document.querySelector('[name="from_currency"]');
 const toSelect = document.querySelector('[name="to_currency"]');
+const ratesByBase = {};
+// const endpoint = 'https://api.apilayer.com/exchangerates_data/convert';
+const endpoint = 'https://api.exchangeratesapi.io/latest';
+var myHeaders = new Headers();
+myHeaders.append('apikey', 'FKS9ifmya70f7cHMToa81DZX6MFM69lt');
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders,
+};
+
+// fetch("https://api.apilayer.com/exchangerates_data/convert?to=to&from=from&amount=amount", requestOptions)
 
 const currencies = {
   USD: 'United States Dollar',
@@ -43,6 +56,25 @@ function generateOptions(options) {
         `<option value="${currencyCode}">${currencyCode} - ${currencyName}</option>`
     )
     .join('');
+}
+
+async function fetchRates(from = 'USD') {
+  const response = await fetch(`${endpoint}?base=${from}`);
+  const rates = await response.json();
+  console.log(rates);
+}
+
+async function convert(amount, from, to) {
+  // First check if we have a cached result
+  if (!ratesByBase[from]) {
+    console.log(`Oh no! We don't have ${from} to conver to ${to}`);
+    const rates = await fetchRates(from);
+    ratesByBase[from] = rates;
+  }
+  // Convert the ammount that was passed in
+  const rate = ratesByBase[from].rates[to];
+  const convertedAmount = amount * rate;
+  return convertedAmount;
 }
 
 const optionsHTML = generateOptions(currencies);
