46a37563c1c4a4141f26481359637287f1014b82
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-19 15:15:35 -0500

    Defines isDown in slider

--- LeetCode/two-sum.js
+++ LeetCode/two-sum.js
@@ -1,4 +1,8 @@
-const nums = [2, 7, 11, 13];
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
 
 function twoSum(nums, target) {
   const numIndices = {};
@@ -17,4 +21,5 @@ function twoSum(nums, target) {
   return [];
 }
 
+const nums = [2, 7, 11, 13];
 console.log(twoSum(nums, 9));
