5712e2f9981d996a8af514da9e9f925bfc0cdf98
Justin Sixsmith <justinsixsmith@gmail.com>
2023-06-21 10:17:55 -0500

    Adds tab functionality

--- JavaScript30/29 - Countdown Timer/scripts-START.js
+++ JavaScript30/29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,15 @@
 let countdown;
-let timerDisplay = document.querySelector('.display__time-left');
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -21,6 +26,31 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  display = `${minutes}:${remainderSeconds}`;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? '0' : ''
+  }${remainderSeconds}`;
+  document.title = display;
   timerDisplay.textContent = display;
 }
+
+function displayEndTime(timeStamp) {
+  const end = new Date(timeStamp);
+  const hours = end.getHours() % 12;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hours}:${
+    minutes < 10 ? '0' : ''
+  }${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach((button) => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
